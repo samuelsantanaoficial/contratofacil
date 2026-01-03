4cef3e5aae12f3cadf1d2e11f47047c482c0575e
samuelsantanaoficial <psicopatadamusica@gmail.com>
2026-01-03 16:38:50 -0300

    Add PWA

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'v1.0.0';
+const ASSETS = [
+	'./',
+	'./index.html',
+	'./main.js',
+	'./manifest.json',
+	'./maskable_icon_x512.png',
+	'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+];
+
+// 1. Instalação: Salva os arquivos no cache
+self.addEventListener('install', (e) => {
+	e.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(ASSETS);
+		})
+	);
+});
+
+// 2. Ativação: Limpa caches antigos se mudar a versão
+self.addEventListener('activate', (e) => {
+	e.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (key !== CACHE_NAME) {
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
+});
+
+// 3. Interceptação: Serve o arquivo do cache se estiver offline
+self.addEventListener('fetch', (e) => {
+	e.respondWith(
+		caches.match(e.request).then((response) => {
+			return response || fetch(e.request);
+		})
+	);
+});
